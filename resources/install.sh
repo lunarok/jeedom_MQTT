85bd87736aafb31970367d54ed4a84ac063f079b
Cédric Dupont <lunarok@gmail.com>
2018-11-01 22:29:20 +0100

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -34,18 +34,18 @@ echo 60 > /tmp/mqtt_dep
 
 phpv=`php --version | head -n 1 | cut -d " " -f 2 | cut -c 1-3`
 
-apt-get -y install php{$phpv}-dev
-if [[ -d "/etc/php/{$phpv}/cli/" && ! `cat /etc/php/{$phpv}/cli/php.ini | grep "mosquitto"` ]]; then
+apt-get -y install php$phpv-dev
+if [[ -d "/etc/php/$phpv/cli/" && ! `cat /etc/php/$phpv/cli/php.ini | grep "mosquitto"` ]]; then
   echo "" | pecl install Mosquitto-alpha
   echo 80 > /tmp/mqtt_dep
-  echo "extension=mosquitto.so" | tee -a /etc/php/{$phpv}/cli/php.ini
+  echo "extension=mosquitto.so" | tee -a /etc/php/$phpv/cli/php.ini
 fi
-if [[ -d "/etc/php/{$phpv}/fpm/" && ! `cat /etc/php/{$phpv}/fpm/php.ini | grep "mosquitto"` ]]; then
-  echo "extension=mosquitto.so" | tee -a /etc/php/{$phpv}/fpm/php.ini
-  service php{$phpv}-fpm restart
+if [[ -d "/etc/php/$phpv/fpm/" && ! `cat /etc/php/$phpv/fpm/php.ini | grep "mosquitto"` ]]; then
+  echo "extension=mosquitto.so" | tee -a /etc/php/$phpv/fpm/php.ini
+  service php$phpv-fpm restart
 fi
-if [[ -d "/etc/php/{$phpv}/apache2/" && ! `cat /etc/php/{$phpv}/apache2/php.ini | grep "mosquitto"` ]]; then
-  echo "extension=mosquitto.so" | tee -a /etc/php/{$phpv}/apache2/php.ini
+if [[ -d "/etc/php/$phpv/apache2/" && ! `cat /etc/php/$phpv/apache2/php.ini | grep "mosquitto"` ]]; then
+  echo "extension=mosquitto.so" | tee -a /etc/php/$phpv/apache2/php.ini
   rm /tmp/mqtt_dep
   echo "Fin installation des dépendances"
   service apache2 restart
